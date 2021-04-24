14e137556ad6ff8777a6f9a97e2a9661d745f450
Sandra Ileana <sandraileana@gmail.com>
2021-04-24 14:02:03 -0500

    Script

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+// var resultTextEl = document.querySelector('#result-text');
+// var resultContentEl = document.querySelector('#result-content');
+// var searchFormEl = document.querySelector('#search-form');
 
 
 // let gfetch = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
@@ -23,29 +23,97 @@ var searchFormEl = document.querySelector('#search-form');
 //        console.log(data);
 //    });
 
-
-// Pulling info from API
-   function searchApi() {
-    
-    fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
-    
-    .then(response => response.json())
-    .then(data => console.log(data));
-     
-       
+      
          
-     }
+//      }
 
- searchApi();
+//  searchApi();
 
- function printResults(resultObj) {
-    console.log(resultObj);
+//  function printResults(resultObj) {
+//     console.log(resultObj);
   
-    // set up `<div>` to hold result content
-    var resultCard = document.createElement('div');
-    resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+//     // set up `<div>` to hold result content
+//     var resultCard = document.createElement('div');
+//     resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
- }
+//  }
+
+
+
+// Functional fetch for Google API  (YA FUNCIONA)
+
+// var GoogleAPIKey = 'AIzaSyBjBtdqp8hFmjH8MUjGWcgMXITnOJ0pZkU';
+
+// var URL = `https://www.googleapis.com/books/v1/volumes?q=search+terms&key=${GoogleAPIKey}`;
+// fetch(URL)
+//    .then(function (response) {
+//        return response.json();
+//    })
+//    .then(function (data) {
+//        console.log(data);
+//    });
+
+
+
+// Google API: subject  (YA FUNCIONA)
+
+// var GoogleAPIKey = 'AIzaSyBjBtdqp8hFmjH8MUjGWcgMXITnOJ0pZkU';
+
+// var URL = `https://www.googleapis.com/books/v1/volumes?q=subject:${}&key=${GoogleAPIKey}`;
+// fetch(URL)
+//    .then(function (response) {
+//        return response.json();
+//    })
+//    .then(function (data) {
+//        console.log(data);
+//    });
+
+
+
+// Google API: author / newest  (YA FUNCIONA)
+
+// var GoogleAPIKey = 'AIzaSyBjBtdqp8hFmjH8MUjGWcgMXITnOJ0pZkU';
+
+// var URL = `https://www.googleapis.com/books/v1/volumes?q=subtitle:fiction&inauthor:keyes&orderBy=newest&key=${GoogleAPIKey}`;
+// // var URL = `https://www.googleapis.com/books/v1/volumes?q=subtitle:${}&inauthor:${}&orderBy=${}&key=${GoogleAPIKey}`;
+// fetch(URL)
+//    .then(function (response) {
+//        return response.json();
+//    })
+//    .then(function (data) {
+//        console.log(data);
+//    });
+
+
+
+// NY Times Books API  (YA FUNCIONA)
+
+// var NYBooksAPIKey = '5DWunwN9QOM7uw5MKvFLET8jlI6cayHP';
+// var URL = `https://api.nytimes.com/svc/books/v3/lists.json?list=tacos&api-key=${NYBooksAPIKey}`;
+// fetch(URL)
+//    .then(function (response) {
+//        return response.json();
+//    })
+//    .then(function (data) {
+//        console.log(data);
+//    });
+
+
+// NY Times Books API: by best sellers (YA FUNCIONA)
+
+var NYBooksAPIKey = '5DWunwN9QOM7uw5MKvFLET8jlI6cayHP';
+var URL = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?=ran&api-key=${NYBooksAPIKey}`;
+fetch(URL)
+   .then(function (response) {
+       return response.json();
+   })
+   .then(function (data) {
+       console.log(data);
+   });
+
+
+
+
 //  for (var i = 0; i < locRes.results.length; i++) {
 //     printResults(locRes.results[i]);
 //   }
